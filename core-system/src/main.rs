59209a02ed8eb0f4c90261446a9ec07df34f0f41
Samir Djelal <ssdjelal@gmail.com>
2022-08-06 16:30:50 +0100

    init

--- core-system/src/main.rs
+++ core-system/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
 	println!("Hello, world!");
+	
+	#[cfg(target_os = "linux")]
+	let plugins = vec!["libfirst_plugin.so", "libsecond_plugin.so"];
+	
+	#[cfg(target_os = "windows")]
 	let plugins = vec!["first_plugin.dll", "second_plugin.dll"];
+	
 	for plugin in plugins {
 		if let Err(e) = call_dynamic(plugin) {
 			eprintln!("Error when running the plugin: {}", e);
